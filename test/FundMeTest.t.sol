85503a7a9257a71538318fba8263fde1cef08a3a
chibyke <chibykepride@gmail.com>
2023-10-16 12:12:58 +0000

    tests and debugging

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -10,6 +10,20 @@ contract FundMeTest is Test {
         fundMe = new FundMe();
     }
     function testMinimumDollarIsFive() public {
+        console.log(fundMe.MINIMUM_USD());
         assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        // assertEq(fundMe.i_owner(), msg.sender);
+            // This test will fail because FundMeTest is the contract that deployed the fundMe address
+                // in the setUp(); function, hence it will be the owner
+            // While in this particular function, msg.sender is whoever is calling the FundMeTest
+                // i.e., Us ->(calls) FundMeTest ->(deploys) fundMe
+                // msg.sender = us
+                // fundMe.i_owner = FundMeTest
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
